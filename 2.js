27ceabbc4dceaad169ebf6c2482dd179efb792eb
StanislavDalimov <stas008@hotmail.com>
2022-10-27 13:36:09 +0400

    hmwrk2

---
+++ 2.js
@@ -0,0 +1,12 @@
+
+function getName() {
+    tempStr = prompt("Привет, Дорогой Друг! Как тебя зовут?")
+    alert("Привет " + (tempStr))
+
+}
+function helloUser(tempName) {
+    alert("Привет, " + (tempName) + "!!!!!")
+}
+getName()
+userName = prompt(" #2 Привет! Как тебя зовут?")
+helloUser(userName)
